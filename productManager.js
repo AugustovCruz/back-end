1f5a71bc316847874524613ba9ba79e01b822e69
AugustovCruz <augusto.tito.96@gmail.com>
2023-08-16 00:36:55 -0300

    Primer desafio

--- productManager.js
+++ productManager.js
@@ -5,13 +5,23 @@ class productManager {
         this.id = 1;
     }
 
+    getProducts () {
+        return this.products
+    }
+
+    getProductById(id) {
+        const product = this.products.find( p => p.id === id)
+        if (product) return console.log('producto encontrado', product)
+        return console.log("Not found")
+    }
+
     addProduct(product){
         if( !product.title || !product.description || !product.price || !product.thumbnail || !product.code | !product.stock  ) {
             console.log("Todos los campos son obligatorios")
             return
         }
 
-        const existProduct = this.product.find( p => p.code === product.code )
+        const existProduct = this.products.find( p => p.code === product.code )
 
         if (existProduct) {
             console.log("El codigo de producto ya existe")
@@ -27,7 +37,30 @@ class productManager {
 
         console.log("Nuevo producto agregado", newProduct)
     }
-
-
-
 }
+
+const manager = new productManager();
+
+console.log(manager.getProducts());
+
+
+
+manager.addProduct({
+    title: "producto prueba",
+    description:"Este es un producto prueba",
+    price:200,
+    thumbnail:"Sin imagen",
+    code:"abc123",
+    stock:25
+})
+
+manager.addProduct({
+    title: "producto prueba2",
+    description:"Este es un producto prueba2",
+    price:2002,
+    thumbnail:"Sin imagen2",
+    code:"abc1232",
+    stock:252
+})
+
+manager.getProductById(2)
