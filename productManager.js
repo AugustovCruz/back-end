55ec2d1575b50ed13941c517fbe3abf0ae7e506b
AugustovCruz <augusto.tito.96@gmail.com>
2023-08-19 20:16:39 -0300

    Segundo desafio

--- productManager.js
+++ productManager.js
@@ -1,27 +1,41 @@
+const fs = require ('fs')
+const filename = './datos.json'
+
 class productManager {
 
     constructor() {
         this.products = [];
         this.id = 1;
+        this.path = filename;
     }
 
     getProducts () {
+        // verifico si los productos existen en los archivos y en tal caso los vuelvo objetos
+        if( fs.existsSync(this.path)) {
+            const contenido = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
+            return contenido 
+        }
+        // en tal caso de que los productos no existan inicializo el array
         return this.products
     }
 
     getProductById(id) {
-        const product = this.products.find( p => p.id === id)
-        if (product) return console.log('producto encontrado', product)
-        return console.log("Not found")
+        //obtengo los productos desde los archivos y busco si existe tal id
+        const productos = this.getProducts()
+        const product= productos.find( p => p.id === id )
+
+        if (product) return console.log('Producto encontrado', product)
+        return console.log("Error: Producto no encontrado en los archivos")
     }
 
-    addProduct(product){
+    async addProduct(product){
+        const productos = this.getProducts()
         if( !product.title || !product.description || !product.price || !product.thumbnail || !product.code | !product.stock  ) {
             console.log("Todos los campos son obligatorios")
             return
         }
 
-        const existProduct = this.products.find( p => p.code === product.code )
+        const existProduct = productos.find( p => p.code === product.code )
 
         if (existProduct) {
             console.log("El codigo de producto ya existe")
@@ -34,9 +48,35 @@ class productManager {
         };
         this.products.push(newProduct)
         this.id++
-
+        await fs.writeFileSync(this.path, JSON.stringify(this.products, null,  '\t'))
         console.log("Nuevo producto agregado", newProduct)
     }
+
+    updateProduct (id, key, value ) {
+        const productos = this.getProducts()
+        // Busco el producto por ID
+        let item = productos.find( el => (el.id === id))
+        // Verifico el campo sea el mismo para poder actualizar
+        if (item && item.hasOwnProperty(key)){
+            item[key]= value
+            fs.writeFileSync(this.path, JSON.stringify(productos, null,'\t' ))
+            console.log('Producto actualizado')
+        } else console.log('No se encontraron los datos para actualizar')
+    }
+
+    async deleteProduct (id)  {
+        const productos= this.getProducts()
+        // Busco el ID para luego poder borrarlo 
+        const productoIndex = productos.findIndex( (item) => item.id === id )
+        if (productoIndex === -1) return console.log('Error: No existe ningun elemento con ese ID')
+        //Borro y actualizo en los archivos 
+        productos.splice(productoIndex, 1)
+        await fs.writeFileSync(this.path, JSON.stringify(productos, null, '\t'))
+        console.log('Producto borrado exitosamente')
+    }
+
+
+
 }
 
 const manager = new productManager();
@@ -63,4 +103,17 @@ manager.addProduct({
     stock:252
 })
 
-manager.getProductById(2)
+manager.addProduct({
+    title: "producto prueba3",
+    description:"Este es un producto prueba3",
+    price:2003,
+    thumbnail:"Sin imagen3",
+    code:"abc12323",
+    stock:2523
+})
+
+manager.getProductById(3)
+
+manager.updateProduct(1, 'stock', 100)
+
+manager.deleteProduct(1)
